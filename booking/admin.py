4d2119c8ee6dd607e50947e5a29ce70c13ef6887
Siyasanga Mbiko <smbiko@gmail.com>
2025-05-09 20:04:21 +0000

    added authentication

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Customer, Booking
+
 
 # Register your models here.
+
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """
+    Customer admin set up, using list display to change
+    the layout of the customer table.
+    """
+    list_display = ('full_name', 'email', 'phone_number')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Booking admin set up, using list display to change
+    the layout of the booking table and list filter so as admin
+    can filter the bookings by status and date.
+    """
+    list_display = ('customer', 'booking_date', 'booking_time',
+                    'number_accompanying', 'booking_status')
+    list_filter = ('booking_status', 'booking_date')
--- booking/urls.py
+++ booking/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 
+
 urlpatterns = [
     path('booking', views.customer_booking, name='booking'),
     path('display_booking', views.display_booking, name='display_booking'),
