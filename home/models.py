2f890620aff4171e38182681e96148851d7fe4d1
Siyasanga Mbiko <smbiko@gmail.com>
2025-05-10 06:58:13 +0000

    fixed template header hrefs

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Contact Form model for posting to the admin panel
+    """
+    email = models.EmailField(blank=False)
+    name = models.CharField(max_length=50, blank=False)
+    message = models.TextField(blank=False)
+    date_posted = models.DateField(default=timezone.now)
+
+    class Meta:
+        verbose_name = "Contact Form Submission"
+
+
+    def __str__(self):
+        return f"{self.name}, {self.email}"    
