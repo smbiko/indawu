48846b9d462608b0c5144d3812dfbdcaa0c161d3
Siyasanga Mbiko <smbiko@gmail.com>
2025-05-10 07:58:29 +0000

    add home views content

--- home/views.py
+++ home/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+
+def home(request):
+    """
+    Home Page
+    """
+    return render(request, "index.html")
+
+
+def contact(request):
+    """
+    Contact Page and Form
+    """
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("thank-you"))
+        else:
+            messages.error(
+                request,
+                "Failed to send message. Please try again. All fields are required.",    
+            )
+    form = ContactForm()
+    context = {"form": form}
+    return render(request, "contact.html", context)        
